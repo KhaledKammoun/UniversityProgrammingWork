fdeff069a6f8d2059fe3e6b423ffc349ea36c9d8
KhaledKammoun <khaledsfax03@gmail.com>
2023-02-10 20:32:05 +0100

    EX3

---
+++ Python-Exercices/TP1/EX3.py
@@ -0,0 +1,19 @@
+from random import *
+# n est un nombre aléatoire
+n = randint(5,20) # or : n = randrange(5,21)
+for i in range(1,n+1) :
+    print('* '*i)
+print()
+for i in range(n,0,-1) :
+    print('* '*i)
+print()
+for i in range(1,n+1) :
+    print('  '*(n-i)+'* '*i)
+print()
+for i in range(n,0,-1) :
+    print('  '*(n-i)+'* '*i)
+print()
+for i in range(n) :
+    for j in range(i+1) :
+        print(chr(97+j%3),end='') # les caractères seront tous imprimés sur la même ligne.
+    print() #retour à la ligne
