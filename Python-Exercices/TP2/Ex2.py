f493414356320078979d4cd958b1adbb6fdc2b9e
KhaledKammoun <khaledsfax03@gmail.com>
2023-02-11 17:20:01 +0100

    ex2

---
+++ Python-Exercices/TP2/Ex2.py
@@ -0,0 +1,16 @@
+mot = "python"
+mot_c = "pythonss"
+mot_c_copy = mot_c
+b = True
+for c in mot :
+    if (mot_c_copy.count(c)>0) :
+        index = mot_c_copy.index(c)
+        mot_c_copy = mot_c_copy[:index] + mot_c_copy[index+1:]
+    else :
+        b = False
+        break
+
+if (b) :
+    print("[{}] est composable à partir de [{}]".format(mot,mot_c))
+else :
+    print("[{}] n'est pas composable à partir de [{}]".format(mot, mot_c))
