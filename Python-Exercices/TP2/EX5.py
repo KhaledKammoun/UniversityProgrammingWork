ca03577c048b5bf1dbed9afb8b31c351f6e14071
KhaledKammoun <khaledsfax03@gmail.com>
2023-02-18 17:54:30 +0100

    EX5

--- Python-Exercices/TP2/EX5.py
+++ Python-Exercices/TP2/EX5.py
@@ -12,4 +12,22 @@ liste_tuple=[(c,len(c)) for c in liste]
 print(liste_tuple)
 #question 3
 ind_mot_court = min(range(len(liste_tuple)), key=lambda i: liste_tuple[i][1])
-print(ind_mot_court)
+print(ind_mot_court,"est l'indice du mot le plus court")
+#question 4
+ch=""
+b=True
+j=0
+while (b!=False and j<len(liste[ind_mot_court])) :
+    i = 1
+    while (b!=False and i<len(liste)) :
+        if (liste[i-1][j]!=liste[i][j]) :
+            b=False 
+            break
+        i+=1
+    if (b) :
+        ch+=liste[0][j]
+    j+=1
+if (ch) :
+    print("racine =",ch)
+else :
+    print("pas de racine commune")
