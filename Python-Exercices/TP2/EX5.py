d5a1c44c83ec9635365c8948aa10171affb12123
KhaledKammoun <khaledsfax03@gmail.com>
2023-02-26 13:02:29 +0100

    EX5

--- Python-Exercices/TP2/EX5.py
+++ Python-Exercices/TP2/EX5.py
@@ -13,19 +13,17 @@ print(liste_tuple)
 #question 3
 # ind_mot_court = min(range(len(liste_tuple)), key=lambda i: liste_tuple[i][1])
 ind_mot_court = 0
-min_len = liste_tuple[0][1]
 for i in range(1,len(liste_tuple)) :
-    if (min_len>liste_tuple[i][1]) :
-        min_len = liste_tuple[i][1]
+    if (liste_tuple[ind_mot_court][1]>liste_tuple[i][1]) :
         ind_mot_court = i
 print(ind_mot_court,"est l'indice du mot le plus court")
 #question 4
 ch=""
 b=True
 j=0
-while (b!=False and j<len(liste[ind_mot_court])) :
+while (b and j<len(liste[ind_mot_court])) :
     i = 1
-    while (b!=False and i<len(liste)) :
+    while (b and i<len(liste)) :
         if (liste[i-1][j]!=liste[i][j]) :
             b=False 
             break
