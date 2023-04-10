a645f9643818a475cca98dd4d56ba930d7f25c91
KhaledKammoun <khaledsfax03@gmail.com>
2023-04-10 23:04:53 +0100

    EX2

---
+++ C-Exercices/S2/TP4/EX2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+void inverse(char*ch , char*ch1){
+    char aux[100] ;
+    strcpy(aux,ch) ;
+    strcpy(ch,ch1) ;
+    strcpy(ch1,aux) ;
+}
+int main(){
+    char ch[100] , ch1[100] ;
+    printf("donner ch : ") ;
+    scanf("%s",&ch) ;
+    printf("donner ch1 : ") ;
+    scanf("%s",&ch1) ;
+    inverse(ch,ch1) ;
+    printf("ch : %s\n",ch) ;
+    printf("ch1 : %s",ch1) ;
+}
