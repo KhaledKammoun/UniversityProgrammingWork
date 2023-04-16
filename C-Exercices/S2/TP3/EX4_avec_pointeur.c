2dcb4b7443f77790d599b5d6edec018914510b5f
KhaledKammoun <khaledsfax03@gmail.com>
2023-04-16 22:50:56 +0100

    EX4

--- C-Exercices/S2/TP3/EX4_avec_pointeur.c
+++ C-Exercices/S2/TP3/EX4_avec_pointeur.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
-int recherche(char *ch , char*ch1){
-    for (char* i = ch1 ; i-ch1<strlen(ch1) ; i++){
-        if (strncmp(ch,i,strlen(ch))==0)
-            return i-ch1 ;
+int recherche(char *ch1 , char*ch2){
+    for (char* i = ch2 ; i-ch2<strlen(ch2) ; i++){
+        if (strncmp(ch1,i,strlen(ch1))==0)
+            return i-ch2 ;
     }
     return -1 ;
 }
 int main(){
-    char *ch , *ch1 ;
-    ch = (char*)malloc(100*sizeof(char)) ;
+    char *ch1 , *ch2 ;
     ch1 = (char*)malloc(100*sizeof(char)) ;
-    printf("donner ch : ") ;
-    scanf("%s",ch) ;
+    ch2 = (char*)malloc(100*sizeof(char)) ;
     printf("donner ch1 : ") ;
     scanf("%s",ch1) ;
-    int test = recherche(ch,ch1) ;
+    printf("donner ch2 : ") ;
+    scanf("%s",ch2) ;
+    int test = recherche(ch1,ch2) ;
     if (test == -1)
-        printf("ch n'est pas incluse dans ch1");
+        printf("ch1 n'est pas incluse dans ch2");
     else 
-        printf("premiere occurrence de ch dans ch1 est dans l'indice : %d", test);
+        printf("premiere occurrence de ch1 dans ch2 est dans l'indice : %d", test);
 }
--- C-Exercices/S2/TP3/EX4_sans_pointeur.c
+++ C-Exercices/S2/TP3/EX4_sans_pointeur.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-int recherche(char *ch , char*ch1){
-    for (int i = 0 ; i<strlen(ch1) ; i++){
-        if (strncmp(ch,ch1+i,strlen(ch))==0)
+int recherche(char *ch1 , char*ch2){
+    for (int i = 0 ; i<strlen(ch2) ; i++){
+        if (strncmp(ch1,ch2+i,strlen(ch1))==0)
             return i ;
     }
     return -1 ;
 }
 int main(){
-    char ch[100] , ch1[100] ;
-    printf("donner ch : ") ;
-    scanf("%s",&ch) ;
+    char ch1[100] , ch2[100] ;
     printf("donner ch1 : ") ;
     scanf("%s",&ch1) ;
-    int test = recherche(ch,ch1) ;
+    printf("donner ch2 : ") ;
+    scanf("%s",&ch2) ;
+    int test = recherche(ch1,ch2) ;
     if (test == -1)
-        printf("ch n'est pas incluse dans ch1");
+        printf("ch1 n'est pas incluse dans ch2");
     else 
-        printf("premiere occurrence de ch dans ch1 est dans l'indice : %d", test);
+        printf("premiere occurrence de ch1 dans ch2 est dans l'indice : %d", test);
 }
