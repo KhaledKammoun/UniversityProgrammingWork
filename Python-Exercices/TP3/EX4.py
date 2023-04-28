a834eba7251ca0f8ea002f2a1a3fe33a18befcbc
KhaledKammoun <khaledsfax03@gmail.com>
2023-04-28 22:21:44 +0100

    EX4

--- Python-Exercices/TP3/EX4.py
+++ Python-Exercices/TP3/EX4.py
@@ -1,11 +1,16 @@
 ch,ch1="aabbc","ddeffa"
 A={c for c in ch}
 B={c for c in ch1}
-if (len(A&B)==0) : # A.intersection(B) {a}
+A_Copy = {c for c in  A if c in B}
+if (len(A_Copy)==0) : # A.intersection(B) {a}
     print("A et B sont disjoints") #ensemble vide
 else :
     print("A et B n'est pas disjoints")
-ensemble= {'ad', 'ae', 'af', 'ba', 'bd', 'be', 'bf', 'ca', 'cd', 'ce', 'cf'}
+ensemble=set()
+alpha=['d','e','f','a']
+for i in range(1,12):
+    ensemble.update([str(chr(97 if i < 4 else 97+i//4)+alpha[(i-1)%4])])
+print(ensemble)
 #question 4
 b = False
 for c in A :
@@ -17,6 +22,5 @@ if (not b) :
 else :
     print("A et B n'est pas disjoints")
 #question 5
-A_Copy = {c for c in A if c not in B}
-
-print("A - B : ",A_Copy)
+A_B = {c for c in A if c not in B}
+print("A - B : ",A_B)
