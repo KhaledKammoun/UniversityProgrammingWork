9d2b2946d1dada0bde64544897ff125658374165
KhaledKammoun <khaledsfax03@gmail.com>
2023-05-09 17:59:06 +0100

    HTML-Project

--- Python-Exercices/TP4/EX3.py
+++ Python-Exercices/TP4/EX3.py
@@ -6,3 +6,18 @@ a,b,c=[int(x) for x in input().split()] #write a,b,c values separated by spaces
 L = f(**{'a': a, 'b': b, 'c': c})
 print(L)
 #question2
+try:
+    with open('input.txt', 'w') as file:
+        file.write('1,2,3\n')
+        file.write('4,5,6\n')
+        file.write('7,8,9\n')
+except FileNotFoundError:
+    print('File not found')
+#question3
+try :
+    with open('input.txt', 'r') as file:
+        for line in file:
+            x = list(map(int,line.split(",")))
+            f(**{'a':x[0],'b':x[1],'c':x[2]}) #print(f(**{'a':x[0],'b':x[1],'c':x[2]}))
+except FileNotFoundError:
+    print('File not found')
