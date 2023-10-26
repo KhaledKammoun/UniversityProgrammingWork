e1b9f5fc51e0c193da6306e4bd9ed7c4a2ab873c
KhaledKammoun <khaledsfax03@gmail.com>
2023-10-26 23:38:41 +0100

    Java

---
+++ D-LSI-ADBD/Java/TP1/ProgrammationOO/ProgrammationOO/src/package1/Chap1Exp1.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class Chap1Exp1 {
+
+	public static void main(String[] args) {
+		System.out.print("Donner a et b : ") ;
+	}
+}
---
+++ D-LSI-ADBD/Java/TP1/unité_D/KhaledKammounDLSIADBD/MonPremierProgramme.java
@@ -0,0 +1,8 @@
+class MonPremierProgramme{
+//ceci est un commentaire sur une ligne
+/* ceci est un commentaire
+Sur plusieurs lignes*/
+public static void main(String [] args){
+System.out.println("ceci est mon premier programme en java");
+}
+}
