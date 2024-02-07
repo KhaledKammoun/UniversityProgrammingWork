1e9c412cad2904f5cfd8753380a11eef9e944221
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-07 11:09:27 +0100

    DLSI-ADBD

---
+++ D-LSI-ADBD/S2/Big_Data_et_architectures_associées/Cours/exerice1.py
@@ -0,0 +1,12 @@
+from mrjob.job import MRJob
+class MRWordFrequencyCount(MRJob) :
+    def mapper(self,_,line) :
+        words = line.split()
+        for word in words :
+            yield word.lower(), 1
+    def reducer(self, key, values) :
+        yield key, sum(values)
+if __name__ == '__main__' :
+    MRWordFrequencyCount.run()
+
+
