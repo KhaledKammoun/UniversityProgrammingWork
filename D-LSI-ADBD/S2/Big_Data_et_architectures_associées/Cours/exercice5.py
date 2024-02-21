cf36621eef44822dd272596a144503bf8cec8440
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-21 11:06:12 +0100

    DLSI-ADBD

---
+++ D-LSI-ADBD/S2/Big_Data_et_architectures_associées/Cours/exercice5.py
@@ -0,0 +1,26 @@
+from mrjob.job import MRJob
+class MRFriendsByAge(MRJob):
+
+    def mapper(self,_, line):
+
+        (ID, name, age, numFriends) = line.split(',')
+
+        yield age, int(numFriends)
+
+    def reducer(self, age, numFriends):
+
+        total = 0
+
+        numElements = 0
+
+        for x in numFriends:
+
+            total += x
+
+            numElements += 1
+
+        yield age, int(total/numElements)
+
+if __name__ == '__main__':
+
+    MRFriendsByAge.run()
