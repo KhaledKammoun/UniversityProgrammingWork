84cf86df24d77d6ab0124fb8a1129ca0419140f1
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-24 21:22:53 +0100

    DLSI-ADBD

---
+++ D-LSI-ADBD/S2/web_dev/ex3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- form -->
+    <form method="post">
+        <label>Name : <input placeholder="Name : " name="name"/></label>
+        <label><input placeholder="Last Name : " name="last_name" /></label>
+        <button type="submit">Envoyer</button>
+    </form>
+    <?php
+
+    ?>
+</body>
+</html>
