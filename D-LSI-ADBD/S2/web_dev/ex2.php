ef4789fbb60789c668ad298eb9fa889e9fc89c88
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-24 01:23:43 +0100

    DLSI-ADBD

--- D-LSI-ADBD/S2/web_dev/ex2.php
+++ D-LSI-ADBD/S2/web_dev/ex2.php
@@ -7,20 +7,62 @@
 </head>
 <body>
     <?php
-        // Check if form is submitted
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // If form submitted, change the value of $main
-            $main = "Name 1";
-        } else {
-            // Initial value of $main
-            $main = "Name 2";
-        }
-    ?>
-
-    <h1><?php echo $main; ?></h1>
+        function createTable($name) {
+            $etudiants=[
+                "ali"=>["html"=>[12,15,14],"java"=>[10,18,13],"angular"=>[14,13.5,17]],
+                "mohamed"=>["html"=>[13,18,15],"java"=>[9,7,3],"angular"=>[8,7.5,7]],
+                "sami"=>["html"=>[17,18,20],"java"=>[14,18,15.5],"angular"=>[13,17.5,18]],
+            ];
+            $table = '
+                <h2>%s</h2>
+                <table border="1">
+                 <tr>
+                  <th>matiere</th><th>note tp</th><th>note ds</th><th>note examen</th><th>moyenne</th>
+                 </tr>
+                  %s
+                 <tr>
+                  <td colspan="4">Moyenne generale</td><td>%s</td>
+                 </tr>
+                 <tr>
+                  <td colspan="4">Mention</td><td>%s</td>
+                 </tr>
+                </table>
+            ' ;
+            
+            $moyenne_generale = 0 ;
     
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <button type="submit">Change Name</button>
-    </form>
+            $table_row = '' ;
+            foreach ($etudiants[$name] as $matiere => $row){
+                $row_moyenne = array_sum($row) / 4 ;
+                $table_row .= sprintf("
+                <tr>
+                 <td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td>
+                </tr>
+                ", $matiere, $row[0], $row[1], $row[2], round($row_moyenne, 2)) ;
+    
+                $moyenne_generale+=round($row_moyenne, 2) ;
+            }
+            $moyenne_generale/=3 ;
+            
+            $mention = "" ;
+            switch($moyenne_generale) {
+                case $moyenne_generale < 10 :
+                    $mention = "refusé" ;
+                    break ;
+                case $moyenne_generale < 12 :
+                    $mention = "assez bien" ;
+                    break ;
+                case $moyenne_generale < 14 :
+                    $mention = "bien" ;
+                    break ;
+                default :
+                    $mention = "trés bien" ;
+                    break ;
+            }
+    
+            echo sprintf($table, $name, $table_row, round($moyenne_generale,2), $mention) ;
+        }
+        createTable("mohamed") ;
+    ?>
 </body>
 </html>
